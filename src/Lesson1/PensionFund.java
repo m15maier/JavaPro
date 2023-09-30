9c65ffbe6f992e190f920466c6bee3274e0731e1
its.m.maier <its.m.maier@gmail.com>
2023-09-30 20:26:07 +0300

    Merge remote-tracking branch 'o/main'
    Signed-off-by: its.m.maier <its.m.maier@gmail.com>

--- src/Lesson1/PensionFund.java
+++ src/Lesson1/PensionFund.java
@@ -1,12 +1,16 @@
 package Lesson1;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PensionFund {
 
     private String name;
     private boolean state;
-    private final String date;
+    private boolean date;
     private List<Worker> persons;
 
 
@@ -18,22 +22,51 @@ public class PensionFund {
         this.persons = persons;
     }
 
-    public PensionFund(String name, boolean state, String date, int users) {
+
+    public PensionFund(String name, boolean state, boolean date, int users) {
         this.name = name;
         this.state = state;
         this.date = date;
         this.persons = persons;
+
+
+//       1. В пенсионный фонд поле добавьте Map, которая будет содержать в себе дни недели (как ключ) и
+//       булеан переменную, как значение. Данное поле будет обозначать, работает ли сегодня пенсионный фонд.
+
+        double basePension;
+        Map<String, Boolean> shedule = new HashMap<String, Boolean>();
+
+        shedule.put("Monday", true);
+        shedule.put("Tuesday", true);
+        shedule.put("Wednesday", true);
+        shedule.put("Thursday", true);
+        shedule.put("Friday", true);
+        shedule.put("Saturday", false);
+        shedule.put("Sunday", false);
+
+
+        LocalDate dateToday = LocalDate.now(); // получаем текущую дату
+        int year = dateToday.getYear();
+        int month = dateToday.getMonthValue();
+        int dayOfMonth = dateToday.getDayOfMonth();
+        DayOfWeek dayOfWeek = dateToday.getDayOfWeek();
+        System.out.println(date);
+        System.out.println(dayOfWeek);
+
     }
 
 
-    public String getName() {
+
+
+
+        public String getName() {
         return name;
     }
     private boolean isTrue() {
         return state;
     }
 
-    private String getDate() {
+    private boolean getDate() {
         return date;
     }
 
@@ -76,7 +109,8 @@ public class PensionFund {
     public double calculatePensionFor(AbleToCalculatePension obj) {
         if (state) {
             return obj.calculatePension();
-        } else {
+        }
+        else {
             System.out.println("Деньги из фонда украли.");
             return 0.0;
         }
@@ -93,4 +127,26 @@ public class PensionFund {
         }
         return sum / persons.size();
     }
+
+
+
+//       2. В логику рассчета пенсии в пенсионном фонде добавьте проверку дня недели.
+//       Если сегодня пенсионный фонд не работает (узнать это можно сравнив текущий день недели и день недели из мапы), тогда пенсия равна 0.
+
+    public double calculatePension(AbleToCalculatePension obj) {
+        if (date == true) {
+            return obj.calculatePension();
+        }
+        else {
+            System.out.println("0 ");
+            return 0.0;
+        }
+    }
+
+
 }
+
+
+
+
+
